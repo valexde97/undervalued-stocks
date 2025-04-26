992c016f6675fd4436b63eeba2d12d87417f74a4
valexde97 <lasco.valentin97@gmail.com>
2025-04-27 01:47:09 +0200

    'lazy'

--- src/components/StockList.tsx
+++ src/components/StockList.tsx
@@ -1,6 +1,6 @@
 import { StockCard } from "./StockCard";
 import { motion } from "framer-motion";
-import styles from './stockList.module.css';
+import styles from "./stockList.module.css";
 
 type Stock = {
   ticker: string;
@@ -19,12 +19,13 @@ export const StockList = ({ stocks }: Props) => (
     {stocks.map((stock, index) => (
       <motion.div
         key={stock.ticker}
-        initial={{ opacity: 0, y: 30 }}
+        initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
+        whileHover={{ scale: 1.02 }}
         transition={{
           type: "spring",
-          stiffness: 70,
-          damping: 12,
+          stiffness: 80,
+          damping: 15,
           delay: index * 0.1,
         }}
         className={styles.cardContainer}
